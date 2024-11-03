ed24b5776b81492653fa7de0371d179e6fc9fe11
RuoyanLi2002 <liruoyan2002@g.ucla.edu>
2024-11-02 23:37:46 -0700

    Gaussian PD Training

--- exps/simple_pcs/main.py
+++ exps/simple_pcs/main.py
@@ -13,12 +13,25 @@ sys.path.append("../../")
 
 from src.utils import instantiate_from_config, collect_data_from_dsets, ProgressBar
 
-
+# python main.py --batch-size 128 --data-config ../../configs/data/imagenet32.yaml --model-config ../../configs/model/pd_64.yaml
+# python main.py --batch-size 128 --data-config ../../configs/data/imagenet32_gaussian.yaml --model-config ../../configs/model/pd_64.yaml
 def resolve_tuple(*args):
     return tuple(args)
 
+noise_type = 'gaussian'
+print(f"noise_type: {noise_type}")
+
+def add_noise(x, noise_type='uniform', noise_level=0.01):
+    if noise_type == 'uniform':
+        noise = torch.rand_like(x) * 2 * noise_level - noise_level
+    elif noise_type == 'gaussian':
+        noise = torch.randn_like(x) * noise_level
+    
+    return x + noise
+
 
 def mini_batch_em_epoch(num_epochs, pc, optimizer, scheduler, train_loader, val_loader, device):
+    print("Mini Batch EM")
     if num_epochs == 0:
         return -1000000.0, -1000000.0, [(-1000000.0, -1000000.0)]
 
@@ -32,7 +45,17 @@ def mini_batch_em_epoch(num_epochs, pc, optimizer, scheduler, train_loader, val_
         total_train_ll = 0.0
         for x in train_loader:
             x = x.to(device, non_blocking = True)
-
+            print(x)
+            print(x.shape)
+            bs = x.shape[0]
+            x = x.reshape(bs, 3, 16, 16)
+            print(x.shape)
+            x = x[:, 0, :, :]
+            print(x.shape)
+            x = x.reshape(bs, -1)
+            print(x.shape)
+            
+            exit()
             optimizer.zero_grad()
 
             lls = pc(x)
@@ -67,7 +90,8 @@ def mini_batch_em_epoch(num_epochs, pc, optimizer, scheduler, train_loader, val_
     return best_ll_pair[0], best_ll_pair[1], all_ll_pairs
 
 
-def full_batch_em_epoch(num_epochs, pc, train_loader, val_loader, device):
+def full_batch_em_epoch(num_epochs, pc, ns, train_loader, val_loader, device):
+    print("Full Batch EM")
     if num_epochs == 0:
         return -1000000.0, -1000000.0, [(-1000000.0, -1000000.0)]
 
@@ -79,11 +103,20 @@ def full_batch_em_epoch(num_epochs, pc, train_loader, val_loader, device):
             t0 = time.time()
             total_train_ll = 0.0
             for x in train_loader:
+                # pc.init_param_flows(flows_memory = 0.0) # Zero out flows
                 x = x.to(device, non_blocking = True)
+                
+                x = x / 127.5 - 1
+                x = add_noise(x, noise_type='uniform', noise_level=0.01)
+                bs = x.shape[0]
+                x = x.reshape(bs, 3, 16, 16)
+                x = x[:, 0, :, :]
+                x = x.reshape(bs, -1)
 
+                # exit()
                 lls = pc(x)
-                pc.backward(x, flows_memory = 1.0)
 
+                pc.backward(x, flows_memory = 1.0, logspace_flows = False, allow_modify_flows = False)
                 total_train_ll += lls.mean().detach().cpu().numpy().item()
 
             pc.mini_batch_em(step_size = 1.0, pseudocount = 0.1)
@@ -94,6 +127,13 @@ def full_batch_em_epoch(num_epochs, pc, train_loader, val_loader, device):
             with torch.no_grad():
                 for x in val_loader:
                     x = x.to(device)
+
+                    x = x / 127.5 - 1
+                    x = add_noise(x, noise_type='uniform', noise_level=0.01)
+                    bs = x.shape[0]
+                    x = x.reshape(bs, 3, 16, 16)
+                    x = x[:, 0, :, :]
+                    x = x.reshape(bs, -1)
 
                     lls = pc(x)
                     total_val_ll += lls.mean().detach().cpu().numpy().item()
@@ -106,6 +146,9 @@ def full_batch_em_epoch(num_epochs, pc, train_loader, val_loader, device):
 
         if aveg_val_ll > best_ll_pair[1]:
             best_ll_pair = (aveg_train_ll, aveg_val_ll)
+
+            pc.update_parameters()
+            juice.save(fname = f"model_gaussian.jpc", model = ns)
 
         all_ll_pairs.append((aveg_train_ll, aveg_val_ll))
 
@@ -130,6 +173,7 @@ def main():
     if args.batch_size > 0:
         data_config["params"]["batch_size"] = args.batch_size
     dsets = instantiate_from_config(data_config)
+
     dsets.prepare_data()
     dsets.setup()
     print("> Dataset prepared")
@@ -144,11 +188,14 @@ def main():
             model_config["params"].pop(k, None)
             model_kwargs[k] = data.cuda()
     print("> Constructing PC...")
+    print(f"model_config: {model_config}")
     ns = instantiate_from_config(model_config, **model_kwargs)
     pc = juice.TensorCircuit(ns)
     pc.print_statistics()
+    
 
     pc.to(device)
+
 
     # Optimizer and scheduler
     optim_config = OmegaConf.load(args.optim_config)
@@ -160,11 +207,11 @@ def main():
     train_loader = dsets._train_dataloader()
     val_loader = dsets._val_dataloader()
 
-    best_tr_ll1, best_vl_ll1, ll_pairs1 = mini_batch_em_epoch(
-        mini_batch_num_epochs, pc, optimizer, scheduler, train_loader, val_loader, device
-    )
+    # best_tr_ll1, best_vl_ll1, ll_pairs1 = mini_batch_em_epoch(
+    #     mini_batch_num_epochs, pc, optimizer, scheduler, train_loader, val_loader, device
+    # )
     best_tr_ll2, best_vl_ll2, ll_pairs2 = full_batch_em_epoch(
-        full_batch_num_epochs, pc, train_loader, val_loader, device
+        full_batch_num_epochs, pc, ns, train_loader, val_loader, device
     )
 
     if best_vl_ll1 > best_vl_ll2:
@@ -177,16 +224,18 @@ def main():
     ll_pairs = ll_pairs1 + ll_pairs2
 
     # Logging
-    fname = os.path.join("logs/", f"{str(uuid.uuid4())}.txt")
-    with open(fname, "w") as f:
-        f.write(f"Data config: {args.data_config}\n")
-        f.write(f"Model config: {args.model_config}\n")
-        f.write(f"Optim config: {args.optim_config}\n")
+    # fname = os.path.join("logs/", f"{str(uuid.uuid4())}.txt")
+    # with open(fname, "w") as f:
+    #     f.write(f"Data config: {args.data_config}\n")
+    #     f.write(f"Model config: {args.model_config}\n")
+    #     f.write(f"Optim config: {args.optim_config}\n")
 
-        f.write(f"Best lls: ({best_tr_ll}, {best_vl_ll})\n\n")
+    #     f.write(f"Best lls: ({best_tr_ll}, {best_vl_ll})\n\n")
 
-        for epoch, (tr_ll, vl_ll) in enumerate(ll_pairs):
-            f.write(f"Epoch {epoch+1} - train LL: {tr_ll:.2f} - validation LL: {vl_ll:.2f}\n")
+    #     for epoch, (tr_ll, vl_ll) in enumerate(ll_pairs):
+    #         f.write(f"Epoch {epoch+1} - train LL: {tr_ll:.2f} - validation LL: {vl_ll:.2f}\n")
+
+    juice.save(fname = f"model_gaussian.jpc", model = ns)
 
 
 if __name__ == "__main__":
